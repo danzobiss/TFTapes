4af5c1b332978bb13b9c97cf58fed363235a6ae4
danzobiss <danzobiss@gmail.com>
2021-05-31 18:03:25 -0300

    Script para enviar pontuação ao banco

--- site/routes/usuarios.js
+++ site/routes/usuarios.js
@@ -53,53 +53,40 @@ router.post('/cadastrar', function(req, res, next) {
     });
 });
 
+router.post('/atualizarPontuacao/idUsuario/:idUsuario/maiorPont/:maiorPont', function(req, res, next) {
+    console.log('Atualizando pontuação máxima');
+    const { idUsuario, maiorPont } = req.params;
 
-/* Verificação de usuário */
-router.get('/sessao/:login', function(req, res, next) {
-    let login = req.params.login;
-    console.log(`Verificando se o usuário ${login} tem sessão`);
+    let instrucaoSql = `update usuario set maiorPont = ${maiorPont} where idUsuario = ${idUsuario}`;
+    console.log(instrucaoSql);
 
-    let tem_sessao = false;
-    for (let u = 0; u < sessoes.length; u++) {
-        if (sessoes[u] == login) {
-            tem_sessao = true;
-            break;
-        }
-    }
+    sequelize.query(instrucaoSql, {
+        model: Usuario
+    }).then(resultado => {
+        console.log(`Encontrados: ${resultado.length}`);
 
-    if (tem_sessao) {
-        let mensagem = `Usuário ${login} possui sessão ativa!`;
-        console.log(mensagem);
-        res.send(mensagem);
-    } else {
-        res.sendStatus(403);
-    }
+        res.json(resultado);
 
+    }).catch(erro => {
+        console.error(erro);
+        res.status(500).send(erro.message);
+    });
 });
 
+router.post('/renderizarPontuacao/idUsuario/:idUsuario', function(req, res, next) {
+    console.log('Renderizando pontuação máxima');
+    const { idUsuario } = req.params;
 
-/* Logoff de usuário */
-router.get('/sair/:login', function(req, res, next) {
-    let login = req.params.login;
-    console.log(`Finalizando a sessão do usuário ${login}`);
-    let nova_sessoes = []
-    for (let u = 0; u < sessoes.length; u++) {
-        if (sessoes[u] != login) {
-            nova_sessoes.push(sessoes[u]);
-        }
-    }
-    sessoes = nova_sessoes;
-    res.send(`Sessão do usuário ${login} finalizada com sucesso!`);
-});
+    let instrucaoSql = `select maiorPont from usuario where idUsuario = ${idUsuario}`;
+    console.log(instrucaoSql);
 
+    sequelize.query(instrucaoSql, {
+        model: Usuario
+    }).then(resultado => {
+        console.log(`Encontrados: ${resultado.length}`);
 
-/* Recuperar todos os usuários */
-router.get('/', function(req, res, next) {
-    console.log('Recuperando todos os usuários');
-    Usuario.findAndCountAll().then(resultado => {
-        console.log(`${resultado.count} registros`);
+        res.json(resultado);
 
-        res.json(resultado.rows);
     }).catch(erro => {
         console.error(erro);
         res.status(500).send(erro.message);
